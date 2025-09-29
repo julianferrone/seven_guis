f1fd7672c16061565a088236279011055b865126
Julian Ferrone <hi@julianferrone.com>
2025-09-29 15:17:43 +1000

    [7] Add flight booker panel (without event handling) to SevenGuis tabbed menu

---
+++ lib/seven_guis/flight_booker.ex
@@ -0,0 +1,81 @@
+defmodule SevenGuis.FlightBooker do
+  use WxEx
+
+  @behaviour :wx_object
+
+  @white {255, 255, 255}
+  @error_red {255, 150, 150}
+
+  def start_link(notebook) do
+    :wx_object.start_link(__MODULE__, [notebook], [])
+  end
+
+  def init([notebook]) do
+    panel = :wxPanel.new(notebook)
+    sizer = :wxBoxSizer.new(wxVERTICAL())
+    :wxWindow.setSizer(panel, sizer)
+
+    # Checkbox
+    flight_option_id = System.unique_integer([:positive, :monotonic])
+
+    flight_option =
+      :wxComboBox.new(
+        panel,
+        flight_option_id,
+        choices: [~c"one-way flight", ~c"return flight"]
+      )
+
+    :wxSizer.add(
+      sizer,
+      flight_option,
+      # flag: wxEXPAND(),
+      proportion: 0,
+      border: 5
+    )
+
+    # Start date
+    start_date_id = System.unique_integer([:positive, :monotonic])
+    start_date = :wxTextCtrl.new(panel, start_date_id)
+
+    :wxSizer.add(
+      sizer,
+      start_date,
+      # flag: wxEXPAND(),
+      proportion: 0,
+      border: 5
+    )
+
+    # Return date
+    return_date_id = System.unique_integer([:positive, :monotonic])
+    return_date = :wxTextCtrl.new(panel, return_date_id)
+
+    :wxSizer.add(
+      sizer,
+      return_date,
+      # flag: wxEXPAND(),
+      proportion: 0,
+      border: 5
+    )
+
+    state = %{
+      panel: panel,
+      flight_option_id: flight_option_id,
+      flight_option: flight_option,
+      start_date_id: start_date_id,
+      start_date: start_date,
+      return_date_id: return_date_id,
+      return_date: return_date
+    }
+
+    {panel, state}
+  end
+
+  # def handle_event(
+  #       {:wx, start_date_id, _, _, {:wxCommand, :command_text_updated, _, _, _}},
+  #       %{start_date_id: start_date_id} = state
+  #     ) do
+  #   text = :wxTextCtrl.getValue(state.start_date)
+  #   temp = Date.from_iso8601(text)
+
+  # end
+end
