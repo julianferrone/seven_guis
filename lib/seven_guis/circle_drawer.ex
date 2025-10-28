dc51e5af462197c1163f1824671f275b7c2f434f
Julian Ferrone <hi@julianferrone.com>
2025-10-28 21:43:49 +1100

    [17] Add undo/redo buttons

--- lib/seven_guis/circle_drawer.ex
+++ lib/seven_guis/circle_drawer.ex
@@ -13,16 +13,35 @@ defmodule SevenGuis.CircleDrawer do
     main_sizer = :wxBoxSizer.new(wxVERTICAL())
     :wxPanel.setSizer(panel, main_sizer)
 
-    # Create window to paint on and make it repaint the whole window on resize
+    # 1. Create buttons for undo/redo
+    button_sizer = :wxBoxSizer.new(wxHORIZONTAL())
+    :wxBoxSizer.add(main_sizer, button_sizer)
+
+    # 1.1. Undo button
+    undo = :wxButton.new(panel, wxID_UNDO())
+    :wxButton.connect(undo, :command_button_clicked)
+    :wxBoxSizer.add(button_sizer, undo)
+
+    # 1.2. Redo button
+    redo = :wxButton.new(panel, wxID_REDO())
+    :wxButton.connect(redo, :command_button_clicked)
+    :wxBoxSizer.add(button_sizer, redo)
+
+    # 2. Create window to paint on and make it repaint the whole window on resize
     canvas = :wxPanel.new(panel, style: wxFULL_REPAINT_ON_RESIZE())
-    :wxSizer.add(main_sizer, canvas, flag: wxEXPAND(), proportion: 1)
     :wxPanel.connect(canvas, :left_down)
     :wxPanel.connect(canvas, :motion)
+    :wxSizer.add(main_sizer, canvas, flag: wxEXPAND(), proportion: 1)
 
     # Force layout
     :wxSizer.layout(main_sizer)
 
-    state = %{canvas: canvas}
+    state = %{
+      canvas: canvas,
+      undo: undo,
+      redo: redo
+    }
+
     {panel, state}
   end
 
