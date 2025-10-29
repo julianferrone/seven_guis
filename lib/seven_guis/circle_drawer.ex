db9dae0df587aeea90b082c5899ded288b63cd1a
Julian Ferrone <hi@julianferrone.com>
2025-10-29 19:33:16 +1100

    [17] Calculate which circle the user has selected by checking the distance between the mouse and circles

--- lib/seven_guis/circle_drawer.ex
+++ lib/seven_guis/circle_drawer.ex
@@ -43,7 +43,11 @@ defmodule SevenGuis.CircleDrawer do
       redo: redo,
       # Event sourcing
       index: 0,
-      commands: []
+      commands: [],
+      # Circle map
+      circles: %{},
+      # is user highlighting a circle? nil if not, index number if yes
+      highlighted: nil
     }
 
     {panel, state}
@@ -53,7 +57,7 @@ defmodule SevenGuis.CircleDrawer do
 
   def handle_event(
         {:wx, _, _, _, {:wxMouse, :left_down, x, y, _, _, _, _, _, _, _, _, _, _}},
-        %{index: index, commands: commands} = state
+        %{index: index, commands: commands, circles: circles} = state
       ) do
     new_circle = %{
       action: :create,
@@ -64,13 +68,62 @@ defmodule SevenGuis.CircleDrawer do
     }
 
     commands = [new_circle | commands]
-    state = %{state | commands: commands, index: index + 1}
+    index = index + 1
+    circles = update(new_circle, circles)
+
+    state = %{state | commands: commands, index: index, circles: circles}
+    {:noreply, state}
+  end
+
+  def handle_event(
+        {:wx, _, _, _, {:wxMouse, :motion, x, y, _, _, _, _, _, _, _, _, _, _}},
+        %{circles: circles} = state
+      ) do
+    mouse_point = %{x: x, y: y}
+    highlighted = selected_circle(mouse_point, circles)
+    state = %{state | highlighted: highlighted}
     {:noreply, state}
   end
 
   def handle_event(request, state) do
     IO.inspect(request: request, state: state)
     {:noreply, state}
+  end
+
+  # __________________ Highlighting Circles __________________
+
+  @type point :: %{x: integer(), y: integer()}
+  @type circle :: %{x: integer(), y: integer(), r: float()}
+  # Indexing into circles
+  @type index :: integer()
+
+  @spec selected_circle(point(), %{index() => circle()}) :: nil | circle()
+  def selected_circle(point, circles) do
+    overlapping_circles =
+      Enum.filter(
+        circles,
+        fn {index, circle} -> {index, is_in_circle(point, circle)} end
+      )
+
+    case Enum.min_by(
+      overlapping_circles,
+      fn {_index, circle} -> distance(point, circle) end,
+      fn -> nil end
+    ) do
+      {index, _circle} -> index
+      nil -> nil
+    end
+  end
+
+  @spec is_in_circle(point(), circle()) :: boolean()
+  defp is_in_circle(point, circle) do
+    # Calculates the distance between the point and the circle origin
+    distance(point, circle) <= circle.r
+  end
+
+  @spec distance(point(), point()) :: float()
+  defp distance(point_a, point_b) do
+    ((point_a.x - point_b.x) ** 2 + (point_a.y - point_b.y) ** 2) ** 0.5
   end
 
   # _____________________ Event Sourcing _____________________
