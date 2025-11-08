b8b05963859934065247f50e2162b11f751a2679
Julian Ferrone <hi@julianferrone.com>
2025-11-08 20:02:24 +1100

    [19] Add parser for expressions

--- lib/seven_guis/cells.ex
+++ lib/seven_guis/cells.ex
@@ -24,6 +24,10 @@ defmodule SevenGuis.Cells do
   @type ast_node_coord :: {:coord, {String.t(), integer()}}
   @type ast_node_number :: {:num, float()}
   @type ast_node_text :: {:text, String.t()}
+  @type ast_node_expr :: {
+          :expr,
+          ast_node_coord() | ast_node_number() | ast_node_function()
+        }
 
   @type function_name :: String.t()
   @type function_arguments :: [ast_node()]
@@ -34,40 +38,6 @@ defmodule SevenGuis.Cells do
           | ast_node_number()
           | ast_node_text()
           | ast_node_function()
-
-  # -------------------- Parsing Formulae --------------------
-
-  # ------------------- Parsing Expressions ------------------
-
-  # ................... Parsing Coordinates ..................
-
-  @spec parse_letters(binary()) :: {binary(), remainder_of_binary :: binary()} | :error
-  def parse_letters(expr) when is_binary(expr) do
-    {word, rest} =
-      Enum.split_while(
-        String.to_charlist(expr),
-        fn x -> x in ?a..?z or x in ?A..?Z end
-      )
-
-    case word do
-      [] -> :error
-      str -> {str, to_string(rest)}
-    end
-  end
-
-  @spec parse_coord(binary()) :: {binary(), remainder_of_binary :: binary()} | :error
-  def parse_coord(expr) when is_binary(expr) do
-    with {row, rest} <- parse_letters(expr),
-         {column, rest2} <- Integer.parse(rest) do
-      {{:coord, row, column}, rest2}
-    else
-      :error -> :error
-    end
-  end
-
-  # ..................... Parsing Numbers ....................
-
-  # ...................... Parsing Text ......................
 
   # ------------------- Parsing Combinators ------------------
 
@@ -99,14 +69,18 @@ defmodule SevenGuis.Cells do
         ) :: parse_result(list(term()))
   defp sequence(text, [], results) do
     {Enum.reverse(results), text}
+    # |> IO.inspect(label: "sequence finished")
   end
 
   defp sequence(text, [parser | parsers], results) do
-    {parsed, rest} = parser.(text)
+    parsed = parser.(text)
+    # |> IO.inspect(label: "sequence")
 
     case parsed do
       :error -> :error
-      success -> sequence(rest, parsers, [success | results])
+      # In the case of parsers we want to ignore, don't append to results
+      {nil, rest} -> sequence(rest, parsers, results)
+      {success, rest} -> sequence(rest, parsers, [success | results])
     end
   end
 
@@ -120,18 +94,149 @@ defmodule SevenGuis.Cells do
   """
   @spec choices(list(parser(term()))) :: parser(term())
   def choices(parsers) do
-    fn text -> choices(text, parsers) end
+    fn
+      text ->
+        choices(text, parsers)
+        # |> IO.inspect(label: "choices finished")
+    end
   end
 
   @spec choices(binary(), list(parser(term()))) :: parse_result(term())
   defp choices(text, []) when is_binary(text), do: :error
 
   defp choices(text, [parser | parsers]) when is_binary(text) do
-    {parsed, rest} = parser.(text)
+    parsed = parser.(text)
+    # |> IO.inspect(label: "choices")
 
     case parsed do
       :error -> choices(text, parsers)
-      success -> {success, rest}
+      {success, rest} -> {success, rest}
     end
   end
+
+  @doc """
+  Creates a parser which throws away the result of the underlying parser.
+
+  Returns `nil` if the underlying parser succeeds, returns `:error` if the
+  underlying parser returns `:error`.
+  """
+  def ignore(parser) do
+    fn text ->
+      result = parser.(text)
+
+      case result do
+        :error -> :error
+        {_result, rest} -> {nil, rest}
+      end
+    end
+  end
+
+  @doc """
+  Creates a parser which errors if the underlying parser doesn't consume
+  all input.
+
+  Returns `:error` if the underlying parser
+  """
+  def parse_all(parser) do
+    fn text ->
+      result = parser.(text)
+      # |> IO.inspect(label: "parse_all")
+
+      case result do
+        :error -> :error
+        {result, ""} -> {result, ""}
+        {_result, _non_empty_string} -> :error
+      end
+    end
+  end
+
+  # -------------------- Parsing Formulae --------------------
+
+  def parse_formula(text) do
+    parsed =
+      choices([
+        parse_all(&parse_float/1),
+        parse_all(&parse_expression/1),
+        # If we don't parse in a float or an expression, parse as text
+        # by pulling the entire string
+        parse_all(fn t -> {{:text, t}, ""} end)
+      ]).(text)
+
+    case parsed do
+      :error -> :error
+      {parsed, _rest} -> parsed
+    end
+  end
+
+  # ------------------- Parsing Expressions ------------------
+
+  def parse_expression(text) do
+    expr =
+      sequence([
+        ignore(parse_char(?=)),
+        choices([
+          &parse_coord/1,
+          &parse_float/1
+          # &parse_function/1,
+        ])
+      ]).(text)
+
+    case expr do
+      {[parsed], ""} -> {{:expr, parsed}, ""}
+      _ -> :error
+    end
+  end
+
+  # ................... Parsing Characters ...................
+
+  def parse_char(char) do
+    fn text ->
+      case String.to_charlist(text) do
+        [^char | rest] -> {char, to_string(rest)}
+        _ -> :error
+      end
+
+      # |> IO.inspect(label: "parse_char")
+    end
+  end
+
+  # ................... Parsing Coordinates ..................
+
+  @spec parse_letters(binary()) :: {binary(), remainder_of_binary :: binary()} | :error
+  def parse_letters(expr) when is_binary(expr) do
+    {word, rest} =
+      Enum.split_while(
+        String.to_charlist(expr),
+        fn x -> x in ?a..?z or x in ?A..?Z end
+      )
+
+    case word do
+      [] -> :error
+      str -> {str, to_string(rest)}
+    end
+  end
+
+  @spec parse_coord(binary()) :: {binary(), remainder_of_binary :: binary()} | :error
+  def parse_coord(expr) when is_binary(expr) do
+    with {row, rest} <- parse_letters(expr),
+         {column, rest2} <- Integer.parse(rest) do
+      {{:coord, row, column}, rest2}
+      # |> IO.inspect(label: "parse_coord")
+    else
+      :error -> :error
+    end
+  end
+
+  # ..................... Parsing Numbers ....................
+
+  def parse_float(expr) do
+    case Float.parse(expr) do
+      {float, rest} -> {{:num, float}, rest}
+      :error -> :error
+    end
+
+    # |> IO.inspect(label: "parse_float")
+  end
+
+  # -------------------- Parsing Functions -------------------
 end
