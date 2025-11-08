43ce1148507acb139a23e327735e2c676d0e1aee
Julian Ferrone <hi@julianferrone.com>
2025-11-08 18:26:33 +1100

    [19) Add parser for coordinates

--- lib/seven_guis/cells.ex
+++ lib/seven_guis/cells.ex
@@ -31,11 +31,43 @@ defmodule SevenGuis.Cells do
 
   @type ast_node ::
           ast_node_coord()
-        | ast_node_number()
-        | ast_node_text()
-        | ast_node_function()
+          | ast_node_number()
+          | ast_node_text()
+          | ast_node_function()
 
   # -------------------- Parsing Formulae --------------------
 
   # ------------------- Parsing Expressions ------------------
+
+  # ................... Parsing Coordinates ..................
+
+  @spec parse_letters(binary()) :: {binary(), remainder_of_binary :: binary()} | :error
+  def parse_letters(expr) when is_binary(expr) do
+    {word, rest} =
+      Enum.split_while(
+        String.to_charlist(expr),
+        fn x -> x in ?a..?z or x in ?A..?Z end
+      )
+
+    case word do
+      [] -> :error
+      str -> {str, to_string(rest)}
+    end
+  end
+
+  @spec parse_coord(binary()) :: {binary(), remainder_of_binary :: binary()} | :error
+  def parse_coord(expr) when is_binary(expr) do
+    with {row, rest} <- parse_letters(expr),
+         {column, rest2} <- Integer.parse(rest) do
+      {{:coord, row, column}, rest2}
+    else
+      :error -> :error
+    end
+  end
+
+  # ..................... Parsing Numbers ....................
+
+  # ...................... Parsing Text ......................
+
+  # ------------------- Parsing Combinators ------------------
 end
