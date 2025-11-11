4db38612f10bae94441302871e9043bdb825b914
Julian Ferrone <hi@julianferrone.com>
2025-11-11 12:54:11 +1100

    [19] Add more AST node types to simplify typehinting

--- lib/seven_guis/cells/ast_node_types.ex
+++ lib/seven_guis/cells/ast_node_types.ex
@@ -1,6 +1,7 @@
 defmodule SevenGuis.Cells.AstNodeTypes do
   @type ast_node_integer :: {:int, integer()}
   @type ast_node_float :: {:float, float()}
+  @type ast_node_number :: ast_node_integer() | ast_node_float()
   @type ast_node_text :: {:text, charlist()}
   @type ast_node_coord :: {:coord, {integer(), integer()}}
   @type ast_node_identifier :: {:ident, charlist()}
@@ -8,15 +9,17 @@ defmodule SevenGuis.Cells.AstNodeTypes do
   @type ast_node_application :: {:appl, ast_node_identifier(), list(expr())}
 
   @type expr ::
-          ast_node_integer()
-          | ast_node_float()
+          ast_node_number()
           | ast_node_coord()
           | ast_node_application()
   @type ast_node_expr :: {:expr, expr()}
 
-  @type ast_node_formula ::
-          ast_node_integer()
-          | ast_node_float()
+  @type ast_node_value() ::
+          ast_node_number()
           | ast_node_text()
+
+  @type ast_node_formula ::
+          ast_node_value()
           | ast_node_expr()
+
 end
