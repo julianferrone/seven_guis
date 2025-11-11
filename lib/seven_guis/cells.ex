20ca96b64df8b530724917120e6ec4ab85e8dcb4
Julian Ferrone <hi@julianferrone.com>
2025-11-11 19:40:08 +1100

    [19] Add text input and grid widgets to Cells page

--- lib/seven_guis/cells.ex
+++ lib/seven_guis/cells.ex
@@ -1,16 +1,87 @@
 defmodule SevenGuis.Cells do
   use WxEx
 
+  alias SevenGuis.Cells.AstNodeTypes, as: AST
+  alias SevenGuis.Cells.ExprGraph
+  alias SevenGuis.Id
+
   @behaviour :wx_object
+
+  @num_rows 100
+  @num_cols 26
+
+  # deep blue
+  @colour_expr {34, 34, 155}
+  # warm black
+  @colour_value {34, 34, 34}
 
   def start_link(notebook) do
     :wx_object.start_link(__MODULE__, [notebook], [])
   end
 
   def init([notebook]) do
-    panel = :wxPanel.new(notebook)
+    # Look:
+    # ┌────────────────────────┐
+    # │ ┌────────────────────┐ │
+    # │ │=SUM(A3, A4)        │ │
+    # │ └────────────────────┘ │
+    # │ ┌──┬─────────────────┐ │
+    # │ │  │                 │ │
+    # │ ├──┼─────────────────┤ │
+    # │ │  │                 │ │
+    # │ │  │                 │ │
+    # │ │  │                 │ │
+    # │ │  │                 │ │
+    # │ │  │                 │ │
+    # │ └──┴─────────────────┘ │
+    # └────────────────────────┘
 
-    state = %{panel: panel}
+    # Layout:
+    # Panel
+    # ┌────────────────────────┐
+    # │Vertical BoxSizer       │
+    # │┌──────────────────────┐│
+    # ││┌────────────────────┐││
+    # │││TextCtrl            │││
+    # ││└────────────────────┘││
+    # ││┌────────────────────┐││
+    # │││Grid                │││
+    # │││                    │││
+    # │││                    │││
+    # │││                    │││
+    # ││└────────────────────┘││
+    # │└──────────────────────┘│
+    # └────────────────────────┘
+
+    panel = :wxPanel.new(notebook)
+    main_sizer = :wxBoxSizer.new(wxVERTICAL())
+    :wxPanel.setSizer(panel, main_sizer)
+
+    # # Add text input cell
+    input = :wxTextCtrl.new(panel, Id.generate_id())
+    :wxBoxSizer.add(main_sizer, input)
+
+    # # Add grid
+    sheet = :wxGrid.new(panel, Id.generate_id())
+    :wxBoxSizer.add(main_sizer, sheet)
+    :wxGrid.createGrid(sheet, @num_rows, @num_cols)
+    :wxGrid.connect(sheet, :grid_select_cell)
+    :wxGrid.connect(sheet, :grid_cell_changed)
+
+    widgets = %{
+      input: input,
+      sheet: sheet
+    }
+
+    expr_graph = ExprGraph.new()
+
+    state = %{
+      panel: panel,
+      widgets: widgets,
+      expr_graph: expr_graph,
+      prev_selected: {0, 0},
+    }
+
     {panel, state}
   end
 
@@ -18,4 +89,10 @@ defmodule SevenGuis.Cells do
     IO.inspect(request: request, state: state)
     {:noreply, state}
   end
+
+  # _____________________ Text Formatting ____________________
+
+  @spec text_colour(AST.ast_node_formula()) :: :wx.wx_colour()
+  def text_colour({:expr, _expr}), do: @colour_expr
+  def text_colour(_value), do: @colour_value
 end
