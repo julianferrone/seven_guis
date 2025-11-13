ac0b95e800b8a03c3fd74fe9d3d436b33feaed29
Julian Ferrone <hi@julianferrone.com>
2025-11-13 16:25:25 +1100

    [19] Convert ranges to lists of coordinates

--- lib/seven_guis/cells/coord.ex
+++ lib/seven_guis/cells/coord.ex
@@ -9,6 +9,17 @@ defmodule SevenGuis.Cells.Coord do
     %{row: row, col: col}
   end
 
+  @spec range_to_coords(t(), t()) :: list(t())
+  def range_to_coords(first, second) do
+    min_row = min(first.row, second.row)
+    max_row = max(first.row, second.row)
+
+    min_col = min(first.col, second.col)
+    max_col = max(first.col, second.col)
+
+    for row <- min_row..max_row, col <- min_col..max_col, do: coord(row, col)
+  end
+
   defimpl List.Chars, for: Coord do
     @spec to_charlist(Coord.t()) :: charlist()
     def to_charlist(coord) do
